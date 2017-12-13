6bdc7a5b942c1485b67268f150ecb173d3b9cd57
StrandedCity <phil@phil-seaton.com>
2017-12-12 23:47:56 -0800

    Delete dead code, allow removal of items from numeric data lists rather than parsing empty strings as zero

--- src/dataFlow/UI/tableValueEnterer.js
+++ src/dataFlow/UI/tableValueEnterer.js
@@ -31,7 +31,6 @@ define([
         this.readOnly = !!readOnly; // false if undefined
 
         _.bindAll(this,
-            "parseInput",
             "dataAsArrayOfArrays",
             "initializeTable",
             "cleanDataArray"
@@ -51,7 +50,6 @@ define([
     TableView.prototype.initializeTable = function(){
 
         var that=this;
-console.log(mapIODataTypeToTableCellTypes[that.dataType])
 
         this.table = new Handsontable(this.$tableContainer.find('#editableTable').get(0), {
             data: that.dataArray,
@@ -72,32 +70,6 @@ console.log(mapIODataTypeToTableCellTypes[that.dataType])
         });
     };
 
-    TableView.prototype.parseInput = function(newData, item){
-        // This whole module is a mess. Obviously, this data-parser should be configurable to parse user input
-        // into the right data type for the component. And it sort of is but.... basically the code just assumes
-        // it's a number and if that fails, tries parsing as a boolean.
-        // There's an exception for strings.
-
-        if (this.stringType) {return newData;}
-
-        var parsedData = item; // can't parse it? do jack.
-
-        var parsingTest = Number(newData);
-        // try parsing as a number
-        if (!_.isNaN(parsingTest)) {
-            parsedData = parsingTest;
-        } else {
-            if (newData.toString().toLowerCase().slice(0,1) === "t") parsedData = true;
-            if (newData.toString().toLowerCase().slice(0,1) === "f") parsedData = false;
-        }
-
-        return parsedData;
-    };
-
-    TableView.prototype.headerHtmlFromPath = function(path){
-        return "<tr><td class='headerRow'>{"+path.join(";")+"}</td></tr>";
-    };
-
     TableView.prototype.dataAsArrayOfArrays = function(tree){
         var outputData = []; // each element is a row
         var outputLabels = []; // each element is a row LABEL
@@ -114,109 +86,6 @@ console.log(mapIODataTypeToTableCellTypes[that.dataType])
 
         return [outputData,outputLabels];
     };
-
-//     TableView.prototype.populate = function(tree,$table){
-//         var that = this,
-//             lastDataPath = [0],
-//             lastDataArray = [];
-//         tree.recurseTree(function(data,node){
-//             // print column headers -- these are not editable:
-//             var path = node.getPath(),
-//                 headerHtml = that.headerHtmlFromPath(path);
-//             $table.append($(headerHtml));
-//
-//             _.each(data,function(item,index){
-//                 var number = _.isNumber(item),
-//                     bool = _.isBoolean(item),
-//                     editable = number || bool,
-//                     cssClass = editable ? "" : "non",
-//                     dataString = item.toString();
-//                 var html = "<tr><td contentEditable='"+editable+"' class='"+cssClass+"editData'>"+dataString+"</td></tr>";
-//                 var $tr = $(html);
-//                 $table.append($tr);
-//                 $tr.find('td').data({
-//                     'd': dataString,
-//                     'f': function(newData){
-// // console.log('new data: ',newData)
-//                         var parsedData = that.parseInput(newData,item);
-// // console.log('parsed data: ',parsedData)
-//                         // replace this item of the array with spec'd data
-//                         data[index] = parsedData;
-//
-//                         $tr.find('td').text(parsedData); // make any standardization kinds of fixes that may be needed
-//                     }
-//                 });
-//             });
-//
-//             lastDataPath = node.getPath();
-//             lastDataArray = data;
-//         });
-//
-//
-//         if (tree.isEmpty()) {
-//             $table.append($(this.headerHtmlFromPath(lastDataPath)));
-//             tree.setDataAtPath(lastDataArray,lastDataPath);
-//         }
-// /////////////
-// // TODO: FIX THIS CODE ITS A MESS!
-// //////////////
-//         console.warn("FIX THIS CODE IT IS MESSY ");
-//         // Make it possible to add a single item at the end:
-//         var extraRowHTML = "<tr><td contentEditable='true' class='addData'>(add value)</td></tr>",
-//             blankRowHTML = "<tr><td contentEditable='true' class='addData'></td></tr>",
-//             $bottomRow = $(blankRowHTML);
-//         $table.append($(extraRowHTML));
-//         $table.append($bottomRow);
-//
-//         // Handle events for data ADDITION
-//         $table.on('focus','.addData',function(){
-//             $(this).text("(add value)");
-//             selectElementContents(this);
-//         }).on('blur','.addData',function(){
-// // console.log('this text: ',$(this).text())
-//             var parsed = that.parseInput($(this).text(),undefined);
-//
-//             if (_.isUndefined(parsed)) $(this).text("(add value)");
-//             else {
-//                 // add to data tree....
-//                 //console.log('adding to data tree:',parsed);
-//                 var index = lastDataArray.length;
-//                 lastDataArray[index] = parsed;
-//                 $(this).text(parsed.toString()); // show the user any corrections that have been made
-//
-//                 // remove "addData" class, change to "editData" since there's now a corresponding entry in the data tree
-//                 $(this).removeClass('addData').addClass('editData').data({
-//                     'd': parsed,
-//                     'f': function(newData){
-//                         var newParsedVal = that.parseInput(newData, parsed);
-//                         lastDataArray[index] = newParsedVal;
-//                     }
-//                 });
-//
-//                 // create a new row for further addition:
-//                 var newRow = $(blankRowHTML);
-//                 $table.append(newRow);
-//             }
-//         });
-//
-//         // Handle events for data EDITING
-//         $table.on('focus','.editData', function() {
-//             selectElementContents(this);
-//         }).on('blur','.editData',function(){
-//             if ($(this).data('d') !== $(this).text()){
-//                 var setCorrespondingDataFunction = $(this).data('f');
-//                 setCorrespondingDataFunction($(this).text());
-//             }
-//         });
-//
-//         function selectElementContents(el) {
-//             var range = document.createRange();
-//             range.selectNodeContents(el);
-//             var sel = window.getSelection();
-//             sel.removeAllRanges();
-//             sel.addRange(range);
-//         }
-//     };
 
     // You might think this is combinable with other overlays, but actually almost all of the overlay code is unique since it
     // relates to the destruction of other stuff in the specific class....
@@ -238,8 +107,10 @@ console.log(mapIODataTypeToTableCellTypes[that.dataType])
     TableView.prototype.cleanDataArray = function () {
         // Return a copy of 'dataArray' that's being edited where empty strings have been removed
         if (this.dataType === ENUMS.OUTPUT_TYPES.NUMBER) {
-            return _.filter(_.unzip(this.dataArray)[0], function(item){
-                return !_.isNull(item);
+            return _.map(_.filter(_.unzip(this.dataArray)[0], function(item){
+                return !_.isEmpty(item) && !_.isNaN(Number(item));
+            }), function (itm){
+                return Number(itm);
             });
         } else if (this.dataType === ENUMS.OUTPUT_TYPES.STRING) {
             // how annoying to find a whole different shape to the data when it's a string type cell...
@@ -248,6 +119,10 @@ console.log(mapIODataTypeToTableCellTypes[that.dataType])
             }),function(stringdata){
                 return stringdata[0];
             });
+        } else if (this.dataType === ENUMS.OUTPUT_TYPES.BOOLEAN) {
+            // TODO ... handle booleans
+            console.warn("NOT IMPLEMENTED: PARSING FOR BOOLEANS");
+            return true;
         }
 
     };
@@ -257,8 +132,8 @@ console.log(mapIODataTypeToTableCellTypes[that.dataType])
         this.$tableContainer.remove();
 
         if (typeof this.callback === "function" && !this.readOnly) {
-            var that = this,
-                cleanData = this.cleanDataArray();
+            var that = this;
+            // TODO: Handle multiple paths for input? Not sure if that's really worth it or not....
             this.data.setDataAtPath(that.cleanDataArray(),[0]);
             this.callback(this.data);
         }
