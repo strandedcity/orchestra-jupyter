c806f55c6f0411367e5258c9678f8b6323ee909e
StrandedCity <phil@phil-seaton.com>
2018-03-04 21:35:00 -0800

    Final improvements for getSessionCookie.sh

--- getSessionCookie.sh
+++ getSessionCookie.sh
@@ -10,34 +10,21 @@ regex=".*token=([0-9a-zA-Z]*) "
 if [[ $OUTPUT =~ $regex ]]
 then
     tokenVal="${BASH_REMATCH[1]}"
-    echo "TOKEN: ${tokenVal}"
+    # echo "TOKEN: ${tokenVal}" #debug
 else
-    echo "NO MATCH"
+    # echo "NO MATCH"
 fi
 
-# TODO: Get this curl working! Currently I'm getting 403s and such. But it's close... should be able to return the headers only, which would include the set-cookie header
-# need to set /etc/hosts such that a curl to <name>.orchestradatascience.com resolves to the jupyter notebook
+cmd="curl 'http://${ORCHESTRA_SUBDOMAIN}.orchestradatascience.com/login?next=%2Ftree%3F'   -H 'content-type: application/x-www-form-urlencoded'  --data 'password=${tokenVal}' -is | grep 'Set-Cookie' | sed -e \"s/^Set-Cookie: //\" | sed -e \"s/HttpOnly; //\" "
 
-# THIS CURL WORKS, BUT I NEED TO MAKE IT WITH THE RIGHT HOST HEADER SO THAT LOGIN DOESN"T ATTACH TO LOCALHOST, WHICH IS USELESS PUBLICLY
-# curl 'http://localhost:8888/login?next=%2Ftree%3F'   -H 'content-type: application/x-www-form-urlencoded'  --data 'password=c5bd837f526c0638c7a1096104d11c843bb636da0338e0f1' -is | grep 'Set-Cookie'
-
-#export ORCHESTRA_SUBDOMAIN
-
-cmd="curl 'http://${ORCHESTRA_SUBDOMAIN}.orchestradatascience.com/login?next=%2Ftree%3F'   -H 'content-type: application/x-www-form-urlencoded'  --data 'password=${tokenVal}' -is | grep 'Set-Cookie'"
-
-echo "CMD: ${cmd}"
+# echo "CMD: ${cmd}" #debug
 
 eval $cmd
 
-# Response = Set-Cookie: username-localhost-8888="2|1:0|10:1520110258|23:username-localhost-8888|44:Y2Q3YTFjMzIxZTI5NGE0ZjhiZTU0ZTIxOWE2OWY3ZWI=|fd3dc9b5b34a53fb56fb2422daaa3db390fe8ef2a8a6ff8e3467605a210dcf80"; expires=Mon, 02 Apr 2018 20:50:58 GMT; HttpOnly; Path=/
+# Response like:
+# Set-Cookie: username-localhost-8888="2|1:0|10:1520110258|23:username-localhost-8888|44:Y2Q3YTFjMzIxZTI5NGE0ZjhiZTU0ZTIxOWE2OWY3ZWI=|fd3dc9b5b34a53fb56fb2422daaa3db390fe8ef2a8a6ff8e3467605a210dcf80"; expires=Mon, 02 Apr 2018 20:50:58 GMT; HttpOnly; Path=/
 
-# username-localhost-8888="2|1:0|10:1520110258|23:username-localhost-8888|44:Y2Q3YTFjMzIxZTI5NGE0ZjhiZTU0ZTIxOWE2OWY3ZWI=|fd3dc9b5b34a53fb56fb2422daaa3db390fe8ef2a8a6ff8e3467605a210dcf80"; expires=Mon, 02 Apr 2018 20:50:58 GMT; HttpOnly; Path=/
-# username-naughty-clarke-orchestradatascience-com="2|1:0|10:1520110018|48:username-naughty-clarke-orchestradatascience-com|44:MDQxM2IyNWExMmZiNDY0OTg3NzlmMTEzMTU1Y2I3MDM=|dac439c32a735d8274a7226f5d1cd27e6a8c5d6edb93661797b932059d88a156"; expires=Mon, 02 Apr 2018 20:46:58 GMT; HttpOnly; Path=/
+# Will become only what I need:
+# username-kickass-curran-orchestradatascience-com="2|1:0|10:1520228006|48:username-kickass-curran-orchestradatascience-com|44:ZDdiZjNkZWQyZGVjNDVkZGFmZTE4MWY2YTg1ZTFkYTA=|da418f197e7134a4c2d655fd4723d3df869c761f4cf93bbecf8eba54cd1bc0e3"; expires=Wed, 04 Apr 2018 05:33:26 GMT; Path=/
 
-#document.cookie='username-localhost-8888="2|1:0|10:1520110258|23:username-localhost-8888|44:Y2Q3YTFjMzIxZTI5NGE0ZjhiZTU0ZTIxOWE2OWY3ZWI=|fd3dc9b5b34a53fb56fb2422daaa3db390fe8ef2a8a6ff8e3467605a210dcf80"; expires=Mon, 02 Apr 2018 20:50:58 GMT; Path=/'
-
-# TODO: changes --
-#- the cookie response needs to have 'username-localhost' subbed out appropriately
-#- I need to make the curl more minimal
-#- XSRF still?
-#- Remove "HttpOnly" from the cookie spec
+# Note the token is never passed to the frontend!
